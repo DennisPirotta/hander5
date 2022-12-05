737d08f8076da8e4b2cfa14d3e3c9871d5104a16
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-12-05 13:43:02 +0100

    update

--- resources/views/transactions/partials/create-transaction-form.blade.php
+++ resources/views/transactions/partials/create-transaction-form.blade.php
@@ -18,14 +18,14 @@
 
     <div>
         <x-input-label for="datetime" :value="__('Date')" />
-        <div class="flex">
-            <div class="relative">
+        <div class="flex flex-row">
+            <div class="relative basis-2/3">
                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </div>
                 <input id="datepicker" name="datetime" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
             </div>
-            <div class="ml-2">
+            <div class="ml-2 basis-1/3">
                 <input type="time" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
         </div>
