457ef6133b2de35ce5fcfd4bf5ea5383cab6385c
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-12-06 08:06:01 +0100

    Revert "update camera test channel"
    This reverts commit 00e7eaac77b5d76d1487772c77ce012c2ddf1f19.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TransactionController;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
@@ -38,9 +37,8 @@ Route::middleware('auth')->group(function () {
 Route::get('/camera',static function(){
     return 'hello';
 });
-Route::post('/camera',static function(Request $request){
+Route::post('/camera',static function(){
     Log::channel('camera')->info('nuova lettura '. Carbon::now());
-    Log::channel('camera')->info($request->all());
     return response('Saved',200);
 });
 
