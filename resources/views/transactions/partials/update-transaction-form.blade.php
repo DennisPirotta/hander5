23a96840489e68dbd73818e10c89ffb277e77500
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-12-09 09:46:19 +0100

    quick fix

--- resources/views/transactions/partials/update-transaction-form.blade.php
+++ resources/views/transactions/partials/update-transaction-form.blade.php
@@ -44,7 +44,7 @@
             </label>
         </li>
         <li>
-            <input type="radio" id="not-payed" name="payed" value="0" class="hidden peer" @if($transaction->payed) checked @endif >
+            <input type="radio" id="not-payed" name="payed" value="0" class="hidden peer" @if(!$transaction->payed) checked @endif >
             <label for="not-payed" class="inline-flex justify-between items-center p-5 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <div class="block">
                     <div class="w-full text-lg font-semibold">{{ __('Not Payed') }}</div>
